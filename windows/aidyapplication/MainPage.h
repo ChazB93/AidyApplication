d939c38a4ffcd5b5214cf9004bb7405e32b1e841
unknown <bizindavyicharles2016@gmail.com>
2023-02-18 22:59:43 +0100

    First commmit

---
+++ windows/aidyapplication/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::aidyapplication::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::aidyapplication::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
